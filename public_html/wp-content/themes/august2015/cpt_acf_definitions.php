81d7a332fab5a2d609e9cf5763344a706af44ad6
Kantoh <leduongkhang@gmail.com>
2015-08-12 17:12:19 +0700

    setting CPT ACF

--- public_html/wp-content/themes/august2015/cpt_acf_definitions.php
+++ public_html/wp-content/themes/august2015/cpt_acf_definitions.php
@@ -10,6 +10,36 @@
 /* ---------------------------------------------------------------------------- */
 /* post type definitions */
 /* ---------------------------------------------------------------------------- */
+add_action('init', 'cptui_register_my_cpts');
+
+function cptui_register_my_cpts() {
+
+    $labels = array(
+        "name" => "Service",
+        "singular_name" => "Service",
+    );
+
+    $args = array(
+        "labels" => $labels,
+        "description" => "",
+        "public" => true,
+        "show_ui" => true,
+        "has_archive" => true,
+        "show_in_menu" => true,
+        "exclude_from_search" => false,
+        "capability_type" => "post",
+        "map_meta_cap" => true,
+        "hierarchical" => false,
+        "rewrite" => array("slug" => "service", "with_front" => true),
+        "query_var" => true,
+        "menu_position" => 26,
+        "menu_icon" => get_template_directory_uri() . '/img/ad-ico/h1.png',
+        "supports" => array("title", "editor", "excerpt"),
+    );
+    register_post_type("service", $args);
+
+// End of cptui_register_my_cpts()
+}
 
 /* ---------------------------------------------------------------------------- */
 /* taxonomy definitions */
@@ -18,3 +48,38 @@
 /* ---------------------------------------------------------------------------- */
 /* custom fields definitions */
 /* ---------------------------------------------------------------------------- */
+if (function_exists("register_field_group")) {
+    register_field_group(array(
+        'id' => 'acf_service',
+        'title' => 'Service',
+        'fields' => array(
+            array(
+                'key' => 'field_55cb01740b031',
+                'label' => 'image',
+                'name' => 'image',
+                'type' => 'image',
+                'save_format' => 'object',
+                'preview_size' => 'thumbnail',
+                'library' => 'all',
+            ),
+        ),
+        'location' => array(
+            array(
+                array(
+                    'param' => 'post_type',
+                    'operator' => '==',
+                    'value' => 'service',
+                    'order_no' => 0,
+                    'group_no' => 0,
+                ),
+            ),
+        ),
+        'options' => array(
+            'position' => 'normal',
+            'layout' => 'no_box',
+            'hide_on_screen' => array(
+            ),
+        ),
+        'menu_order' => 0,
+    ));
+}
