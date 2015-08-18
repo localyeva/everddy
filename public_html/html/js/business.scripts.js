13700bd11146718c5a3191f4ad10c566c3b2d9a7
haudv <haudv@evolableasia.vn>
2015-08-18 10:10:47 +0700

    merit page

--- public_html/html/js/business.scripts.js
+++ public_html/html/js/business.scripts.js
@@ -2,10 +2,7 @@ jQuery( function( $ ) {
 	
   	$(".bg").css( "background-size", "cover" );
 	
-  	$("a[href='#go-to-top']").click(function() {
-		$("html, body").animate({ scrollTop: 0 }, "slow");
-		return false;
-	});
+  	$('img[usemap]').rwdImageMaps();
 
 });
 
