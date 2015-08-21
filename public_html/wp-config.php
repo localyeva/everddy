7aade4270ab6527ad186dc406e2b18a6c9c02a96
haudv <haudv@evolableasia.vn>
2015-08-21 09:46:27 +0700

    chang mysql pass

--- public_html/wp-config.php
+++ public_html/wp-config.php
@@ -22,7 +22,7 @@ define('DB_NAME', 'everddy');
 define('DB_USER', 'root');
 
 /** MySQL database password */
-define('DB_PASSWORD', '123456');
+define('DB_PASSWORD', 'root');
 
 /** MySQL hostname */
 define('DB_HOST', '127.0.0.1');
