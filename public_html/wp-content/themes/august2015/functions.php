41be977fa24862f41ea5cec92ef63ea2abe63d2b
Kantoh <leduongkhang@gmail.com>
2015-09-03 11:04:18 +0700

    Fix Bugs

--- public_html/wp-content/themes/august2015/functions.php
+++ public_html/wp-content/themes/august2015/functions.php
@@ -153,12 +153,14 @@ function remove_menus() {
     $arr_roles = array('administrator');
 
     if (in_array($role, $arr_roles)) {
+        remove_menu_page('index.php');                  //Dashboard
         remove_menu_page('edit.php');                   //Posts
         remove_menu_page('upload.php');                 //Media
         remove_menu_page('edit-comments.php');          //Comments
         remove_menu_page('plugins.php');                //Plugins
         remove_menu_page('users.php');                  //Users
         remove_menu_page('tools.php');                  //Tools
+        remove_menu_page('options-general.php');        //Settings
     } else {
         remove_menu_page('index.php');                  //Dashboard
         remove_menu_page('edit.php');                   //Posts
