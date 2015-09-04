a27d9b7fcb673a29e77f2447c77f6657b7c8b19d
Kantoh <leduongkhang@gmail.com>
2015-09-04 15:21:18 +0700

    Fix Bugs

--- public_html/wp-content/themes/august2015/functions.php
+++ public_html/wp-content/themes/august2015/functions.php
@@ -220,10 +220,10 @@ function remove_menus() {
     if (in_array($role, $arr_roles)) {
         remove_menu_page('index.php');                  //Dashboard
         remove_menu_page('edit.php');                   //Posts
-        remove_menu_page('upload.php');                 //Media
+//        remove_menu_page('upload.php');                 //Media
         remove_menu_page('edit-comments.php');          //Comments
         remove_menu_page('plugins.php');                //Plugins
-        remove_menu_page('users.php');                  //Users
+//        remove_menu_page('users.php');                  //Users
         remove_menu_page('tools.php');                  //Tools
         remove_menu_page('options-general.php');        //Settings
     } else {
--- public_html/wp-content/themes/august2015/part_bottom_contact.php
+++ public_html/wp-content/themes/august2015/part_bottom_contact.php
@@ -11,10 +11,10 @@
     </div><!-- grid -->
     <div class="grid grid-pad">                        
         <div class="col-1-2 text-right">
-            <a href="<?php echo bloginfo('url') ?>/contact" class="btn btn-primary square-btn btn-foot-contact1 pad-btn">人材をお探しの企業様</a>
+            <a href="<?php echo bloginfo('url') ?>/contact" class="btn btn-primary square-btn btn-foot-contact1 pad-btn">お問い合わせはこちら</a>
         </div>
         <div class="col-1-2 text-left">
-            <a href="<?php echo bloginfo('url') ?>/contact-job" class="btn btn-warning square-btn btn-foot-contact2 pad-btn">お仕事をお探しの方</a>
+            <a href="<?php echo bloginfo('url') ?>/contact-job" class="btn btn-warning square-btn btn-foot-contact2 pad-btn">求職のご相談はこちら</a>
         </div>
     </div><!-- grid -->
 </div> <!-- home-cta -->
