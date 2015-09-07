e1c838e5dca7508e9f81a10244c6272f3a9423cf
haudv <haudv@evolableasia.vn>
2015-09-07 14:47:16 +0700

    fix bugs

--- public_html/wp-content/themes/august2015/header.php
+++ public_html/wp-content/themes/august2015/header.php
@@ -26,15 +26,6 @@
           <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
           <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
         <![endif]-->
-        <style type="text/css">
-            @font-face
-            {
-                font-family: 'mplus-1p-regular';
-                src: url("http://mplus-fonts.sourceforge.jp/webfonts/mplus-1p-regular.ttf")
-                    format("truetype");
-            }
-
-        </style>
         <?php        
         global $active_menus;
         wp_head(); 
